a24fddefd35040447597239c8561f7a13a69c4c5
Denis Park <des1991@gmail.com>
2019-06-08 15:56:11 +0300

    Fix method tests_by_level for User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    Test.joins('JOIN tests_users ON tests.id = tests_users.test_id 
-      JOIN users ON users.id = tests_users.user_id')
-      .where(tests: { level: level })
+    Test.joins('JOIN tests_users ON tests.id = tests_users.test_id')
+        .where(tests_users: { user_id: self.id },level: level)
   end
 end
