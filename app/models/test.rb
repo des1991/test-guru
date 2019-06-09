8434f62e2111fb105732f79a7a3e69014f92ffa7
Denis Park <des1991@gmail.com>
2019-06-09 23:49:40 +0300

    Task 5 done.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+
   def self.titles_by_category(title)
     joins('JOIN categories ON tests.category_id = categories.id')
       .where(categories: { title: title })
