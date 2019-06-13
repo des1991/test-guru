d3ebb61046be2aa2455e36060df3ed3396fa8f43
Denis Park <des1991@gmail.com>
2019-06-13 12:59:59 +0300

    Task 7 done.

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,47 @@
+class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index new create]
+  before_action :find_question, only: %i[show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    @questions = @test.questions
+  end
+
+  def show
+    @question
+  end
+
+  def new
+  end
+
+  def create
+    @test.questions.create(question_params)
+
+    redirect_to test_questions_path
+  end
+
+  def destroy
+    @question.destroy
+
+    redirect_to test_questions_path(test_id: @question.test_id)
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question not found!'
+  end
+end
