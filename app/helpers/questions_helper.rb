691ad3443019012e55d42f6b02838bc655b319b0
Denis Park <des1991@gmail.com>
2019-06-17 12:35:01 +0300

    Task 8 done.

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,13 @@
 module QuestionsHelper
+  def question_header(question)
+    if question.new_record?
+      action = 'Create new'
+      title = @test.title
+    else
+      action = 'Edit'
+      title = question.test.title
+    end
+
+    "#{action} <#{title}> question"
+  end
 end
